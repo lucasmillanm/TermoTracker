1b65e634d37fc311662dbd651018f0e250a1567a
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-02-01 09:00:48 +0100

    add mainactivity to github

---
+++ app/src/main/java/com/elpatron/termotracker/MainActivity.java
@@ -0,0 +1,17 @@
+package com.elpatron.termotracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
