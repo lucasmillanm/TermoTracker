c2a2e04e941ce933b3bd107bc1d143c8169a8177
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-02-02 08:29:37 +0100

    add notification move code to functions

--- app/src/main/java/com/elpatron/termotracker/MainActivity.java
+++ app/src/main/java/com/elpatron/termotracker/MainActivity.java
@@ -1,12 +1,20 @@
 package com.elpatron.termotracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -33,11 +41,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             this.setTextDisplayed("Temperature not available");
             isTemperatureSensorAvailable = false;
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notification", "channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         this.setTextDisplayed(sensorEvent.values[0] + " ℃");
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(MainActivity.this, "notification");
+        if (sensorEvent.values[0] >= 30) {
+            this.setColor("#DC583C");
+            this.handleNotification(notification, "It’s already more than 30 degrees outside, wear shorts");
+        } else if (sensorEvent.values[0] <= 0) {
+            this.setColor("#3C9BDC");
+            this.handleNotification(notification, "It’s below 0 degrees, it’s freezing, wear something warm");
+        } else {
+            textDisplayed.setTextColor(Color.GRAY);
+        }
     }
 
     @Override
@@ -66,9 +89,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             sensorManager.registerListener(this, temperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
+
     private void unregisterListener() {
         if (isTemperatureSensorAvailable) {
             sensorManager.unregisterListener(this);
         }
+    }
+
+    private void setColor(String color) {
+        textDisplayed.setTextColor(Color.parseColor(color));
+    }
+
+    private void handleNotification(NotificationCompat.Builder notification, String contentText) {
+        notification.setContentTitle("TempoTracker");
+        notification.setContentText(contentText);
+        notification.setSmallIcon(R.drawable.ic_launcher_foreground);
+        notification.setAutoCancel(true);
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        managerCompat.notify(0, notification.build());
     }
 }
